1aeabefa3fe1d36d34bd6ccc091d14c1fbe3de7f
ruanovsky <ruanvic21@gmail.com>
2026-02-22 14:08:54 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { GlobalStyle } from "./styles/global";
+
+
+export function App() {
+  return (
+    <>
+     <h1>Dt money</h1>
+     <GlobalStyle />
+    </>
+  )
+}
+
---
+++ src/styles/global.ts
@@ -0,0 +1,50 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+	:root{
+		--blue-light: #17c5e5;
+		--blue-dark: #0D98B2;
+
+		--red: #e52e4d;
+
+		--background: #121214;
+		--shape-primary: #323238;
+		--gray-700: #323238;
+		--gray-800: #29292E;
+
+		--text-primary: #FFFFFF;
+
+	}
+
+
+	*{
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}     
+
+	html {
+		@media(max-width: 1080px){
+			font-size: 93.75%;
+		}
+
+		@media(max-width: 720px){
+			font-size: 87.5%;
+		}
+	}
+
+	body {
+		background: var(--background);
+		-webkit-font-smoothing: antialiased;
+		color: var(--text-primary);
+	}
+
+	button {
+		cursor: pointer;
+	}
+
+	[disabled]{
+		opacity: 0.6;
+	}
+  
+`
